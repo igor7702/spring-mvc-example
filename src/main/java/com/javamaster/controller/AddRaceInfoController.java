754aed32075c8df99c9246e15e921133c279e17d
Stanislav Tarasov <iluvstrwrs@mail.ru>
2021-10-21 07:33:01 +0300

    Update whole project

--- src/main/java/com/javamaster/controller/AddRaceInfoController.java
+++ src/main/java/com/javamaster/controller/AddRaceInfoController.java
@@ -1,6 +1,7 @@
 package com.javamaster.controller;
 
 import com.javamaster.dao.GetRaceFromDB;
+import com.javamaster.dao.SetRaceToDB;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,19 +12,11 @@ public class AddRaceInfoController {
 
     @GetMapping("/addRaceInfo")
     public String home(@RequestParam(required = false) String numberOfTheRaceWeek,
-                       Model model, String dataOfTheRace, String countryRace) {
+                       Model model, String yearOfTheRace, String countryOfTheRace,
+                       String cityOfTheRace) {
 
-        GetRaceFromDB raceFromDBObj;
-        raceFromDBObj = new GetRaceFromDB(numberOfTheRaceWeek, dataOfTheRace);
-
-        countryRace = raceFromDBObj.getCountryRace();
-
-        System.out.println("Country of the race is : " + countryRace);
-        System.out.println("Week number of the race is: " + numberOfTheRaceWeek);
-        System.out.println("Year of the race is: " + dataOfTheRace);
-        model.addAttribute("countryRace", countryRace);
-        model.addAttribute("numberOfTheRaceWeek", numberOfTheRaceWeek);
-        model.addAttribute("dataOfTheRace", dataOfTheRace);
+        SetRaceToDB raceToDBObj;
+        raceToDBObj = new SetRaceToDB(numberOfTheRaceWeek, yearOfTheRace, countryOfTheRace, cityOfTheRace);
 
         return "answerAddRaceInfo_page";
     }
---
+++ src/main/java/com/javamaster/dao/SetRaceToDB.java
@@ -0,0 +1,61 @@
+package com.javamaster.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class SetRaceToDB {
+    int number;
+    Boolean answerAbouSet;
+
+    void show() {
+        System.out.println("Number: " + number);
+    }
+
+    public Boolean setRaceToDB() {
+        return answerAbouSet;
+    }
+
+    public SetRaceToDB(String raceWeek, String raceYear, String raceCountry, String raceCity) {
+        number = 100;
+        Connection c = null;
+        Statement stmt = null;
+        int idRace;
+        boolean answerAbouSet;
+        String qwRaceWeek, qwRaceYear,qwRaceCountry, qwraceCity;
+        show();
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager
+                    .getConnection("jdbc:postgresql://localhost:5432/shoes_shop",
+                            "postgres", "12");
+            c.setAutoCommit(false);
+            System.out.println("Opened database successfully");
+
+            stmt = c.createStatement();
+
+            try {
+                idRace = Integer.parseInt(raceYear+raceWeek);
+                System.out.println(idRace);
+            } catch (NumberFormatException e) {
+                System.err.println("Wrong format for the idRace!");
+            }
+
+            String sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "
+                    + "VALUES (7, 'Paul', 32, 'California', 20000.00 );";
+            stmt.executeUpdate(sql);
+
+            stmt.close();
+            c.commit();
+            c.close();
+        } catch (Exception e) {
+            answerAbouSet = false;
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("Operation done successfully");
+        answerAbouSet = true;
+    }
+}
+
