fdf4b436fac31667e45f2d1f06733295b2c454ea
Stanislav Tarasov <iluvstrwrs@mail.ru>
2021-10-23 09:02:37 +0300

    Update whole project

--- src/test/java/com/javamaster/SpringMvcApplicationTests.java
+++ src/test/java/com/javamaster/SpringMvcApplicationTests.java
@@ -1,19 +1,52 @@
 package com.javamaster;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.RepetitionInfo;
+import org.junit.jupiter.api.TestReporter;
+import org.junit.jupiter.api.extension.ParameterResolver;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringMvcApplicationTests {
+	@BeforeAll
+	static void beforeAll(TestInfo testInfo) {
+		System.out.println("Before all take parameters. Started: " + testInfo.getDisplayName());
+	}
+
+	@BeforeAll
+	static void beforeAll(TestReporter testReporter) {
+		testReporter.publishEntry("myEntry", "myValue");
+	}
+
+	@BeforeAll
+	static void beforeAll(TestInfo testInfo, TestReporter testReporter) {
+		testReporter.publishEntry("myOtherEntry", testInfo.getDisplayName());
+	}
+
+	@BeforeEach
+	void beforeEach(){
+		System.out.println("Run my test!");
+	}
+
+	@BeforeEach
+	void beforeEach(TestInfo testInfo) {
+
+	}
 
 	@Test
 	void contextLoads() {
 	}
 
+	@DisplayName("JUnit5 - GetRaceFromDB()")
 	@Test
 	void GetRaceFromDB(){
 	}
 
+	@DisplayName("JUnit5 - SetRaceToDB()")
 	@Test
 	void SetRaceToDB(){
 	}
