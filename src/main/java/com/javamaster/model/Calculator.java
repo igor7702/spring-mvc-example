163a1cbbcfefa7c0fd88e8d4987c6134f5668ea7
Stanislav Tarasov <iluvstrwrs@mail.ru>
2021-10-24 07:37:54 +0300

    Update whole project

---
+++ src/main/java/com/javamaster/model/Calculator.java
@@ -0,0 +1,7 @@
+package com.javamaster.model;
+
+public class Calculator {
+    public int add(int a, int b){
+        return a + b;
+    }
+}
--- src/test/java/com/javamaster/SpringMvcApplicationTests.java
+++ src/test/java/com/javamaster/SpringMvcApplicationTests.java
@@ -51,4 +51,6 @@ class SpringMvcApplicationTests {
 	void SetRaceToDB(){
 	}
 
+
+
 }
